0ef5b28452543fb68bcfdaa0f40d218f37fd9ceb
jerome loi <gorhgorh@gmail.com>
2013-07-05 00:18:51 +0200

    base structure public files, express server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node standAloneProcessing.js
+web: node server.js
--- cat.js
+++ cat.js
@@ -19,6 +19,39 @@
 //
 //
 //
-// thank you johnny-five
+// kudos to johnny-five
 
+
+// this file is the robot itself, or its mind, you decide,
+// and it is javascript all the way.
+
+
+var five = require("johnny-five"),  // johnny-five, enable us to talk to sir arduino
+    board = new five.Board(),       // initialise a board instance that will contain instance of our hardware
+    servo,
+    servoY,
+    laser,
+    onlineLed;
+
+//
+board.on("ready", function() {
+
+    // in this section we will create instances for our hardware
+    // This example allows the button module to
+    // create a completely default instance
+    laser     = new five.Led(12);     // cat mesmeriser
+    onlineLed = new five.Led(13);     // a led to test the board
+    servoX    = new five.Servo(10);   // servo for the X axis
+    servoY    = new five.Servo(9);    // servo for the Y axis
+
+    // inject our hardware in the Repl, so we can talk with
+    // then in the command
+    board.repl.inject({
+        servoX:servoX,
+        servoY:servoY,
+        laser:laser,
+        onlineLed: onlineLed
+    });
+
+});
 
