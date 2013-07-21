2f532852663194b84e1563085011d7d1fa78e744
Jérôme Loï <gorhgorh@gmail.com>
2013-07-21 23:16:08 +0200

    update help and binary

--- cat.js
+++ cat.js
@@ -48,7 +48,7 @@ board.on('ready', function() {
     // in this section we will create instances for our hardware
     // This example allows the button module to
     // create a completely default instance
-    laser     = createLaser(12);     // cat mesmeriser created by a factory function to add initial state
+    laser     = createLaser(12);     // cat mesmeriser created by a factory function to add an initial state
     onlineLed = new five.Led(13);     // a led to test the board
     servoX    = new five.Servo(10);   // servo for the X axis
     servoY    = new five.Servo(9);    // servo for the Y axis
