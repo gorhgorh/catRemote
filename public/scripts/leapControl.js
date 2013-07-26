a599a4cee5fc14f4336067496c2787427b777903
Jérôme Loï <gorhgorh@gmail.com>
2013-07-27 00:38:08 +0200

    basic leap motion support, updated help

--- public/scripts/leapControl.js
+++ public/scripts/leapControl.js
@@ -14,29 +14,31 @@ Leap.loop(controllerOptions, function(frame) {
 
     i++;
     // track only 40frame/s
-    if (i%6 === 0) {
+    if (i%3 === 0) {
+        console.log('frame');
         if (frame.hands.length > 0){
         
-            posX = scale(frame.hands[0].palmPosition[0],150);
-            posY = scale(frame.hands[0].palmPosition[1],150);
-            console.log(frame.hands[0].palmPosition[1]);
-            if (posX >= 1) {
-                posX = 1;
-            }
-            else if (posX <= 0) {
-                posX = 0;
-            }
-            if (posY >= 1) {
-                posY = 1;
-            }
-            else if (posY <= 0) {
-                posY = 0;
-            }
+            posX = scaleX(frame.hands[0].palmPosition[0],300);
+            posY = scaleY(frame.hands[0].palmPosition[1],300);
+            console.log (posX + '/' + posY);
+            //console.log(frame.hands[0].palmPosition[1]);
+            // if (posX >= 1) {
+            //     posX = 1;
+            // }
+            // else if (posX <= 0) {
+            //     posX = 0;
+            // }
+            // if (posY >= 1) {
+            //     posY = 1;
+            // }
+            // else if (posY <= 0) {
+            //     posY = 0;
+            // }
 
-            socket.emit("send", {
-                sliderX: posX,
-                sliderY: posY
-        });
+            // socket.emit("send", {
+            //     sliderX: posX,
+            //     sliderY: posY
+            // });
     }
         i=0;
         //console.log ("cycle")
@@ -65,10 +67,12 @@ Leap.loop(controllerOptions, function(frame) {
 
     // convert position info in a 0 to 1 int so we can use a 
     // normalized value with differents actuators
-    function scale(val, valMax) {
+    function scaleX(val, valMax) {
         return (val + valMax) / valMax * 2;
     }
-
+    function scaleY(val, valMax) {
+        return val /valMax ;
+    }
     
         
 });
